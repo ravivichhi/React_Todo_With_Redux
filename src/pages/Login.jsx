06ce019fac5a96c3b9917f523cedf47cb0bf1df8
ravi vichhi <vichhiravi18@gmail.com>
2024-01-25 00:38:28 +0530

    some changes

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginComponant from '../componants/Login'
+
+export default function Login() {
+  return (
+    <>
+      <LoginComponant />
+    </>
+  )
+}
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignupComponant from '../componants/Signup'
+
+export default function Signup() {
+  return (
+    <>
+      <SignupComponant />
+    </>
+  )
+}
